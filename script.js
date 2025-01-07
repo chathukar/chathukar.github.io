61993868bb57ff38cffaa98f0ff6279db3585af0
chathukar <ckarunasiri123@gmail.com>
2025-01-06 21:58:21 -0500

    Clear chat history when exiting browser, 2

--- script.js
+++ script.js
@@ -247,7 +247,10 @@ function setupPresenceHandling(userRef, roomNumber) {
     document.addEventListener('visibilitychange', () => {
         if (document.hidden) {
             console.log("Page hidden");
-            userRef.remove();
+            if (currentRoom && currentUserRef) {
+                currentUserRef.remove();
+                checkAndClearEmptyRoom(currentRoom);
+            }
         } else {
             console.log("Page visible");
             // Force rejoin if we have a current room
@@ -303,6 +306,7 @@ let currentUserRef = null; // Add this with your other global variables
 
 // Add this function to check and clear empty rooms
 function checkAndClearEmptyRoom(roomNumber) {
+    console.log("Checking if room is empty:", roomNumber);
     const roomRef = firebase.database().ref(`rooms/${roomNumber}`);
     
     roomRef.child('users').once('value')
